21e9f1131defa76de870b0eb8e71cefc42cf66f9
davansep <priscila.davanse@gmail.com>
2021-09-29 11:41:16 -0300

    [Feature Priscila - Task III initial - Create Project Spring - 29.09.21]

---
+++ Sinergia/src/test/java/br/com/sinergia/Sinergia/SinergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sinergia.Sinergia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
