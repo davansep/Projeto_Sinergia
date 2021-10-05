2dc600aa5a4016fff464b0ca2c4d3061622d154e
davansep <priscila.davanse@gmail.com>
2021-10-05 10:45:26 -0300

    [@davansep - Task 6: CREATE interface UsuarioRepository] 2021.10.05

---
+++ Sinergia/src/main/java/br/com/sinergia/Sinergia/repository/UsuarioRepository.java
@@ -0,0 +1,35 @@
+package br.com.sinergia.Sinergia.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.com.sinergia.Sinergia.model.Usuario;
+
+/**
+ * @author Priscila
+ * @version 1.0
+ */
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	/**
+	 * Metodo utilizado para realizar pesquisa pela coluna email da tabela usuario
+	 * 
+	 * @param email
+	 * @return Optional com Usuario
+	 * @author Priscila
+	 * @since 1.0
+	 */
+	public Optional<Usuario> findByEmail(String email);
+
+	/**
+	 * Metodo utilizado para realizar pesquisa pela coluna nome da tabela usuario
+	 * 
+	 * @param nome
+	 * @return List com Usuario
+	 * @author Priscila
+	 * @since 1.0
+	 */
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+}
