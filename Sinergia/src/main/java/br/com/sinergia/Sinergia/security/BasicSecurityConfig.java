429b6206866c5f8bd95e7f026cb2b84e785c9839
Amanda Amorim <88997158+amandioca@users.noreply.github.com>
2021-10-18 18:36:56 -0300

    Revert "Heroku"

--- Sinergia/src/main/java/br/com/sinergia/Sinergia/security/BasicSecurityConfig.java
+++ Sinergia/src/main/java/br/com/sinergia/Sinergia/security/BasicSecurityConfig.java
@@ -28,7 +28,6 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.POST, "/usuarios/cadastro").permitAll()
 				.antMatchers(HttpMethod.POST, "/usuarios/login").permitAll()
 				.antMatchers(HttpMethod.PUT, "/usuarios/atualizar").permitAll()
-				.antMatchers(HttpMethod.OPTIONS).permitAll()
 				.anyRequest().authenticated().and()
 				.httpBasic().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.cors().and().csrf().disable();
